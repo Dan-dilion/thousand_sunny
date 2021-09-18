4228e080778f91543750964bf46a987cc0669391
Dan <dan@waxworlds.org>
2021-09-18 17:55:10 +0100

    Transitions working
    Got basic transition working - I decided to go with the
    react-transition-group library in the end as the M-ui transitions were
    less configurable.
    
    Also downgraded M-ui to V4 - M-ui v5 came out in the last couple of days
    and It looks like it's got loads of bugs! I tried to get it working for
    a while and then decided to go with v4 for this project.
    
    Set browser tab icon

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import { Container, Paper, Button, Typography, Zoom } from '@material-ui/core';
+import { Transition } from 'react-transition-group';
+
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
+  const [ isMounted, setIsMounted ] = useState(false);
+
+  const duration = 1000;
+
+  const defaultStyle = {
+    position: "relative",
+    transition: `opacity ${duration}ms ease-out, transform ${duration}ms ease-out, left ${duration}ms ease-out, top ${duration}ms ease-out`,
+    opacity: 0,
+  }
+
+  const transitionLeft = {
+    entering: { opacity: 1, transform: "scale(0.99)", left: "2vw", top: ".5vw"},
+    entered:  { opacity: 1, transform: "scale(1)", left: 0, top: 0},
+    exiting:  { opacity: 0, transform: "scale(1)", left: 0, top: 0},
+    exited:  { opacity: 0, transform: "scale(0.99)", left: "-2vw", top: ".5vw"},
+  };
+
+  const handleClick = () => {
+    setIsMounted(prev => !prev)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <Container>
+      <Button onClick={handleClick}>click me</Button>
+      <Transition in={isMounted} timeout={0}>
+        {state => (
+          <Paper style={{...defaultStyle, ...transitionLeft[state]}}>
+            <Typography component="h1">Hello!</Typography>
+          </Paper>
+        )}
+      </Transition>
+    </Container>
   );
 }
 
