d67ccdbc391d13dfb7af09dd666f9e81f2512166
Dan <dan@waxworlds.org>
2021-09-22 20:17:19 +0100

    Forgot to remove this!

--- src/components/FeaturedDestinations/FeaturedDestinations.js
+++ src/components/FeaturedDestinations/FeaturedDestinations.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Container, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Image from 'material-ui-image'
 
 import Animate from '../Animate/';
 
