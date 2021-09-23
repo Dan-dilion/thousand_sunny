bd2ae12ce81ddb35685b213747121d8b62ad610e
Dan <dan@waxworlds.org>
2021-09-23 16:42:35 +0100

    Viewport Trigger Works

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import TrackVisibility from 'react-on-screen';
 
 import ExploreAndTravel from '../../components/ExploreAndTravel';
 import NewWayToExplore from '../../components/NewWayToExplore';
 import FeaturedDestinations from '../../components/FeaturedDestinations';
 import GuidesByThousand from '../../components/GuidesByThousand';
-import Testimonials from '../../components/Testimonials/';
+import Testimonials from '../../components/Testimonials';
+import TrendingStories from '../../components/TrendingStories';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,20 +39,47 @@ const useStyles = makeStyles(theme => ({
 
   },
   Testimonials: {
-    
+
+  },
+  TrendingStories: {
+
   }
 }))
 
 const Home = (props) => {
   const classes = useStyles();
 
+  const [ isNewWayToExploreMounted, setIsNewWayToExploreMounted ] = useState(0);
+  const [ isFeaturedDestinationsMounted, setIsFeaturedDestinationsMounted ] = useState(0);
+  const [ isGuidesByThousandMounted, setIsGuidesByThousandMounted ] = useState(0);
+  const [ isTestimonialsMounted, setIsTestimonialsMounted ] = useState(0);
+  const [ isTrendingStoriesMounted, setIsTrendingStoriesMounted ] = useState(0);
+
+  const VISIBILITY_OFFSET = 300;
+
   return(
     <Container className={classes.root}>
       <ExploreAndTravel className={classes.ExploreAndTravel} isHomeMounted={props.isHomeMounted} />
-      <NewWayToExplore className={classes.NewWayToExplore} isNewWayToExploreMounted={props.isHomeMounted} />
-      <FeaturedDestinations className={classes.FeaturedDestinations} isFeaturedDestinationsMounted={props.isHomeMounted} setHeaderPosition={props.setHeaderPosition} />
-      <GuidesByThousand className={classes.GuidesByThousand} isGuidesByThousandMounted={props.isHomeMounted} />
-      <Testimonials className={classes.Testimonials} isTestimonialsMounted={props.isHomeMounted} />
+
+      <TrackVisibility offset={VISIBILITY_OFFSET}>
+        {({ isVisible }) => <NewWayToExplore className={classes.NewWayToExplore} isNewWayToExploreMounted={isVisible} /> }
+      </TrackVisibility>
+
+      <TrackVisibility offset={VISIBILITY_OFFSET}>
+        {({ isVisible }) => <FeaturedDestinations className={classes.FeaturedDestinations} isFeaturedDestinationsMounted={isVisible} setHeaderPosition={props.setHeaderPosition} /> }
+      </TrackVisibility>
+
+      <TrackVisibility offset={VISIBILITY_OFFSET}>
+        {({ isVisible }) => <GuidesByThousand className={classes.GuidesByThousand} isGuidesByThousandMounted={isVisible} /> }
+      </TrackVisibility>
+
+      <TrackVisibility offset={VISIBILITY_OFFSET}>
+        {({ isVisible }) => <Testimonials className={classes.Testimonials} isTestimonialsMounted={isVisible} /> }
+      </TrackVisibility>
+
+      <TrackVisibility offset={VISIBILITY_OFFSET}>
+        {({ isVisible }) => <TrendingStories className={classes.TrendingStories} isTrendingStoriesMounted={isVisible} /> }
+      </TrackVisibility>
     </Container>
   )
 }
